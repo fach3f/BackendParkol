e0dcb6e290ea5692f44b22306d8ac90aaf657545
Kreusna Bayu <kreusna.bayu@mhs.itenas.ac.id>
2023-12-25 23:38:01 +0700

    Update parkol.sql

--- Database/parkol.sql
+++ Database/parkol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 25 Des 2023 pada 16.41
+-- Waktu pembuatan: 25 Des 2023 pada 17.37
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.0.30
 
@@ -24,13 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `kendaraan`
+--
+
+CREATE TABLE `kendaraan` (
+  `id_kendaraan` int(10) NOT NULL,
+  `jenis` enum('mobil','motor') NOT NULL,
+  `jam_masuk` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lantai_parkir`
+--
+
+CREATE TABLE `lantai_parkir` (
+  `id_lantai` int(11) NOT NULL,
+  `nomer_lantai` enum('1','2') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `login`
 --
 
 CREATE TABLE `login` (
   `id_login` int(10) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -42,9 +65,34 @@ INSERT INTO `login` (`id_login`, `username`, `password`) VALUES
 (2, 'fahrizal', '1234567'),
 (3, 'bayu', '1234567');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `parkir`
+--
+
+CREATE TABLE `parkir` (
+  `id_parkir` int(10) NOT NULL,
+  `id_kendaraan` int(10) NOT NULL,
+  `id_lantai` int(10) NOT NULL,
+  `jam_keluar` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`id_kendaraan`);
+
+--
+-- Indeks untuk tabel `lantai_parkir`
+--
+ALTER TABLE `lantai_parkir`
+  ADD PRIMARY KEY (`id_lantai`);
 
 --
 -- Indeks untuk tabel `login`
@@ -53,14 +101,56 @@ ALTER TABLE `login`
   ADD PRIMARY KEY (`id_login`);
 
 --
+-- Indeks untuk tabel `parkir`
+--
+ALTER TABLE `parkir`
+  ADD PRIMARY KEY (`id_parkir`),
+  ADD UNIQUE KEY `foreign key` (`id_kendaraan`),
+  ADD UNIQUE KEY `id_lantai` (`id_lantai`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  MODIFY `id_kendaraan` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `lantai_parkir`
+--
+ALTER TABLE `lantai_parkir`
+  MODIFY `id_lantai` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `login`
 --
 ALTER TABLE `login`
   MODIFY `id_login` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `parkir`
+--
+ALTER TABLE `parkir`
+  MODIFY `id_parkir` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `lantai_parkir`
+--
+ALTER TABLE `lantai_parkir`
+  ADD CONSTRAINT `lantai_parkir_ibfk_1` FOREIGN KEY (`id_lantai`) REFERENCES `parkir` (`id_lantai`);
+
+--
+-- Ketidakleluasaan untuk tabel `parkir`
+--
+ALTER TABLE `parkir`
+  ADD CONSTRAINT `parkir_ibfk_1` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraan` (`id_kendaraan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
